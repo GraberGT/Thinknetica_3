663719fdf1266129c919fcddcda24c8fabdb5b8b
GraberGT <rogerpluscom@gmail.com>
2021-08-09 00:41:59 +0600

    first commit, active storage

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,6 +65,8 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.after(:all) { FileUtils.rm_rf("#{Rails.root.join('tmp/storage')}") }
+  end
   Shoulda::Matchers.configure do |config|
     config.integrate do |with|
       with.test_framework :rspec
